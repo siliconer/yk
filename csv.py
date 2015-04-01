1b11583cd48c5e99980c252c37dcd6b712ff93fb
xgg <siliconexplorer1@gmail.com>
2015-04-01 17:16:40 +0800

    去掉csv.py路径依赖  && 整合read_fp_growth到csv_read()

--- csv.py
+++ csv.py
@@ -2,6 +2,7 @@
 import _csv
 import sys
 import codecs
+import os 
 reload(sys)                      
 sys.setdefaultencoding('utf-8')   
 from  fp_growth import find_frequent_itemsets #导入fp_growth这个算法包里的函数发现频繁项
@@ -59,7 +60,10 @@ def targetvalue_make1(target_one,target_two,target_three,target_four):
     return targetvalue
 
 def csv_read():  #核心的函数，
-	with open('E:/yk/test/my/yk--my.csv', 'rb') as csvfile: #读取csv 文件.
+	current_dir = os.getcwd()
+	csv_file = current_dir + '/my/yk--my.csv'
+	write_file = current_dir + '/yangka.txt'
+	with open(csv_file, 'rb') as csvfile: #读取csv 文件.
 		csvreader = _csv.reader(csvfile, delimiter=' ', quotechar='|')
 		negative_chinese = '\xe5\x90\xa6'
 		postive_chinese = '\xe6\x98\xaf'
@@ -67,7 +71,7 @@ def csv_read():  #核心的函数，
 		targetvalue = ''#初始化为空字符.
 		index_number = 0 
 		yangka_data = [['0' for i in range(0,2)] for j in range(0,90000)]
-		fpgrowth_yangka = codecs.open("E:/yk/test/my/yangka.txt","w")  #定义要把发现出来的数据写入的文件. 其中w代表写.
+		fpgrowth_yangka = codecs.open(write_file,"w")  #定义要把发现出来的数据写入的文件. 其中w代表写.
 
 		for row in csvreader:
 			element = str(', '.join(row)) 
@@ -96,21 +100,19 @@ def csv_read():  #核心的函数，
 			# 生成数值之后赋值给yangka_data这个数组，列1为是否出账生成这个的状态变化#的数据，列2为目标变化生成的数据. index_number每次加1 作为一个浮标
 			yangka_data[index_number][0] = billvalue
 			yangka_data[index_number][1] = targetvalue
-			index_jnumber = index_number + 1
+			index_number = index_number + 1
 		# print yankga_data
 		# 这是核心部分，需要在运行这个程序之前安装fp_growth这个频繁项挖掘的算法包，其返回的itemset 就是挖掘处来的每个数组集如‘['00000', '30288'] ’，support 为出现次数‘2782’,它们共同组成生成到文件里的记录‘['00000', '30288'] 2782’，下面的数字500 是挖掘出来的记录出现的最低次数，比如像'‘['00000', '30288'] ’这种状态如果出现次数低于500 就不考虑，超过500 就考虑，在这里它出现了'2782'次，所以就会考虑了.
 		for (itemset,support) in find_frequent_itemsets(yangka_data,500,True):
 			print>>fpgrowth_yangka,itemset,support #把这些挖掘得到的记录 生成到文件yangka.txt里.
-# 
-			# break		 	
-# read_fpgrowth_yangka 的功能是清理最开始生成的文件，去除'['30028'] 4428' 这类格式的记录.
-def read_fpgrowth_yangka():
-	file = 'E:/yk/test/my/yangka.txt'
-	fpgrowth_yangka = codecs.open("E:/yk/test/my/yangka_new.txt","w")
-	fp_data = open(file)
+		
+	new_write_file = current_dir + '/yangka_new.txt'
+	fpgrowth_yangka_new = codecs.open(new_write_file,"w")
+	fp_data = open(write_file)
 	for data in fp_data:
 		if len(str(data.split(']')[0])) > 8:
-			print>>fpgrowth_yangka,data.replace('\n', ' ').replace('\r', '')
-# csv_read() # 去掉前面的'#'注释符就可以执行这个函数,进行频繁项发现.
-read_fpgrowth_yangka() #清理文件yangka.txt
+			print>>fpgrowth_yangka_new,data.replace('\n', ' ').replace('\r', '')
+			# break		 	
+
+csv_read() # 去掉前面的'#'注释符就可以执行这个函数,进行频繁项发现.
 
